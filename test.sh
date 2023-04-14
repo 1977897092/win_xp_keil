60b800b43722a9255454cfff43a17648ee43d22a
dengyanjun <1977897092@qq.com>
2023-04-14 12:38:21 +0800

    标题: 修改进度条及编译结果显示 修改原因: 1.原方案无进度显示所以等待过程无法判断是否超时或者编译异常 2.keil编译结果无法查看，无法判断虚拟机是否正常编译代码 修改方案: 1增加编译过程中内容显示 关联仓库: 备注:

--- test.sh
+++ test.sh
@@ -1,13 +1,51 @@
 #!/bin/bash
-for ((i=1;i<=20;i++))
-do
-    if (( i > 10 ));then
-        if  VBoxManage guestcontrol "xp-keil" stat "C:\Keil_v5\UV4\UV4.exe" --username Administrator | grep "found";then
-            echo "virtual machine is online"
-        else
-            sleep 1s
-        fi
-    else
-        sleep 1s
-    fi
-done
+
+ratio_display()
+{
+
+    local ratio_length=50
+    local delay_time_max=60
+    local mark=''
+    local ratio
+    local delay_time
+    for ((i=0;i<=$delay_time_max;i++))
+    do
+        delay_time=`awk 'BEGIN{printf "%.2f\n",'$delay_time_max'/'$ratio_length'}'`
+        ratio=`awk 'BEGIN{printf "%.2f\n",'$i'*'100'/'$delay_time_max'}'`
+        # echo $delay_time
+        # echo $ratio
+        printf "progress:[%-60s]%.2f%%\r" "${mark}" "${ratio}"
+        sleep $delay_time
+        mark="#${mark}"
+    done
+    echo
+}
+
+
+ratio_display
+# c=`awk -v x=2.45 -v y=3.123 'BEGIN{printf "%.2f\n",x*y}'`
+# echo $c
+
+
+# mark=''
+# for ((ratio=0;${ratio}<=60;ratio+=1))
+# do
+#         printf "progress:[%-60s]%d%%\r" "${mark}" "${ratio}"
+#         sleep 1.66
+#         mark="#${mark}"
+# done
+
+
+#轮询虚拟机
+# for ((i=1;i<=20;i++))
+# do
+#     if (( i > 10 ));then
+#         if  VBoxManage guestcontrol "xp-keil" stat "C:\Keil_v5\UV4\UV4.exe" --username Administrator | grep "found";then
+#             echo "virtual machine is online"
+#         else
+#             sleep 1s
+#         fi
+#     else
+#         sleep 1s
+#     fi
+# done
